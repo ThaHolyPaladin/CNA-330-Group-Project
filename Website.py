04333f5a12bf684db854d8745bedb796a52b7fe3
ThaHolyPaladin <65267796+ThaHolyPaladin@users.noreply.github.com>
2020-12-02 13:50:11 -0800

    Update V.01
    changing the default code to the xcel and information we will be using.

--- Website.py
+++ Website.py
@@ -1,6 +1,6 @@
 import pandas
 
-tables = pandas.read_excel ('https://www.eia.gov/dnav/pet/hist_xls/EMD_EPD2DXL0_PTE_R5XCA_DPGw.xls', sheet_name='Data 1',
+tables = pandas.read_excel ('https://www.eia.gov/dnav/pet/hist_xls/EMD_EPD2D_PTE_R50_DPGw.xls', sheet_name='Data 1',
                             header = 2, index_col='Date')
 
 print(tables)
