01e796e6542dbe85569ff6e849b40f99ad29a62a
Mike <65267796+ThaHolyPaladin@users.noreply.github.com>
2020-12-10 22:07:38 -0800

    Update for big overhaul
    reorganizing code to be able to plot data for graph

---
+++ graph for fuel.py
@@ -0,0 +1,30 @@
+# CNA 330 Group Project fuel prices throughout the years. How the pandemic has affected gas prices#
+# Collaborated with Eric, Dillion, Igor, Vlado, Mohammad,
+# Help from Justin Ellis & Zac Rubin #
+# 12/9/2020 #
+# This script pulls from the eia website and stores fuel prices into a database. It takes the information and compiles into a xcel sheet.
+# CNA 330 Group Project - Michael Horton, Carlos 
+
+from sqlalchemy import create_engine # https://www.geeksforgeeks.org/how-to-convert-pandas-dataframe-into-sql-in-python/
+import pandas as pd
+from matplotlib import pyplot as plt
+
+hostname="127.0.0.1"
+uname="root"
+pwd=""
+dbname="sql-group-project"
+
+engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
+            .format(host=hostname, db=dbname, user=uname, pw=pwd))
+
+df = pd.read_sql_table('Fuel', engine)
+
+print(df)
+
+plt.figure(figsize=(24, 24))
+plt.plot(df['Date'], df['Diesel_Price'])
+plt.xlabel('Date')
+plt.xticks(rotation=90)
+plt.ylabel('Diesel Price')
+plt.title('Diesel Over Time', fontsize=16)
+plt.show()
