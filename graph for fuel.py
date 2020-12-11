4561c9be30a2179768eef04f68810d7928a27ed7
Mike <65267796+ThaHolyPaladin@users.noreply.github.com>
2020-12-11 10:11:13 -0800

    Final update for Graph
    Fixed spelling errors and was able to import data successfully.

--- graph for fuel.py
+++ graph for fuel.py
@@ -5,37 +5,25 @@
 # This script pulls from the eia website and stores fuel prices into a database. It takes the information and compiles into a xcel sheet.
 # CNA 330 Group Project - Michael Horton, Carlos
 
-from sqlalchemy import create_engine # https://www.geeksforgeeks.org/how-to-convert-pandas-dataframe-into-sql-in-python/
+from sqlalchemy import create_engine
 import pandas as pd
-from matplotlib import pyplot as plt
-import socket
-
-socket.getaddrinfo('localhost', 3306)
-
-config = {
-  'user': 'root',
-  'password': 'root',
-  'host': '127.0.0.1',
-  'database': 'sql-group-project',
-  'raise_on_warnings': True,
-}
+import matplotlib.pyplot as plt
 
 hostname="127.0.0.1"
 uname="root"
 pwd="root"
 dbname="sql-group-project"
-
 engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
-            .format(host="127.0.0.1", db=sql-group-project, user=root, pw="root"))
+            .format(host=hostname, db=dbname, user=uname, pw=pwd))
 
-df = pd.read_sql_table('Fuel', engine)
+df = pd.read_sql_table('fuel', engine)
 
 print(df)
 
 plt.figure(figsize=(24, 24))
-plt.plot(df['Date'], df['Diesel_Price'])
+plt.plot(df['Date'], df['Price'])
 plt.xlabel('Date')
 plt.xticks(rotation=90)
-plt.ylabel('Diesel Price')
-plt.title('Diesel Over Time', fontsize=16)
+plt.ylabel('Fuel Price')
+plt.title('Fuel Over Time', fontsize=16)
 plt.show()
